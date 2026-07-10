5acdfef8337d37365b19ecffa48e3f965b8fee2e
maizatulmuzafar-prog <maizatulmuzafar@gmail.com>
2026-07-11 05:23:42 +0800

    Update script.js

--- script.js
+++ script.js
@@ -35,4 +35,29 @@ setInterval(function(){
 
     document.getElementById("slide").src = photos[index];
 
-}, 3000);
+}, 3000);function createHeart(){
+
+    const heart = document.createElement("div");
+
+    heart.className = "heart";
+
+    heart.innerHTML = "💙";
+
+    heart.style.left = Math.random() * 100 + "%";
+
+    heart.style.animationDuration = 
+    (Math.random() * 3 + 3) + "s";
+
+    document.querySelector(".hearts").appendChild(heart);
+
+
+    setTimeout(() => {
+
+        heart.remove();
+
+    }, 6000);
+
+}
+
+
+setInterval(createHeart, 300);
